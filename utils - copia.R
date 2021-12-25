fe6597742b2deaabb9f5bbcaf00ebc98977d2439
gloknar <mipeeeerro@gmail.com>
2021-12-25 16:12:47 +0100

    Update utils - copia.R

--- utils - copia.R
+++ utils - copia.R
@@ -1,24 +1,3 @@
-##gmtPathways is copied from fgsea package.
-gmtPathways <- function(gmt.file) {
-    pathwayLines <- strsplit(readLines(gmt.file), "\t")
-    pathways <- lapply(pathwayLines, tail, -2)
-    names(pathways) <- sapply(pathwayLines, head, 1)
-    pathways
-}
-
-## get_os is copied from https://github.com/r-lib/rappdirs/blob/master/R/utils.r#L1
-get_os <- function() {
-  if (.Platform$OS.type == "windows") { 
-    "win"
-  } else if (Sys.info()["sysname"] == "Darwin") {
-    "mac" 
-  } else if (.Platform$OS.type == "unix") { 
-    "unix"
-  } else {
-    stop("Unknown OS")
-  }
-}
-
 ##from :  https://github.com/mikelove/DESeq2/blob/master/R/core.R
 estimateSizeFactorsForMatrix <- function(counts, locfunc=stats::median,
                                          geoMeans, controlGenes) {
@@ -54,27 +33,3 @@ estimateSizeFactorsForMatrix <- function(counts, locfunc=stats::median,
   }
   sf
 }
-
-
-##calculate how many pathways of one gene involved.
-num_of_pathways <- function (gmtfile,overlapgenes){
-  pathways <- gmtPathways(gmtfile)
-  pathway_names <- names(pathways)
-  filter_pathways <- list()
-  for (p in pathway_names){
-    genes <- pathways[[p]]
-    common_genes <- intersect(genes,overlapgenes)
-    if(length(common_genes>=5)){
-      filter_pathways[[p]] <- common_genes
-    }
-  }
-  
-  all_genes <- unique(as.vector(unlist(filter_pathways)))
-  gene_times <- data.frame(num =rep(0,length(all_genes)),row.names = all_genes)
-  for(p in pathway_names){
-    for(g in filter_pathways[[p]]){
-      gene_times[g,"num"] = gene_times[g,"num"]+1
-    }
-  }
-  gene_times
-} 
