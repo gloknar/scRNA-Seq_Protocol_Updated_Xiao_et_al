402a81c2b0091a4cd808da2e1f0581e7889dbc5d
gloknar <mipeeeerro@gmail.com>
2021-12-30 23:59:37 +0100

    Update scRNA_pathway_activity.R

--- 5-PathwayActivity/scRNA_pathway_activity.R
+++ 5-PathwayActivity/scRNA_pathway_activity.R
@@ -79,65 +79,91 @@ mean_expression_noshuffle <- matrix(NA, nrow = length(nombres_pathways),
 View(mean_expression_shuffle)
 View(mean_expression_noshuffle)
 
+View(matriz_TPM_norm[genes_compartidos, ])
 
-for(ruta_metab in nombres_pathways){
+for (ruta_metab in nombres_pathways) {
   
   # Comprobamos cuántos genes de la ruta metabólica están presentes tanto en el
   # archivo pathways.gmt como en la matriz TPM normalizada, y si dicha ruta
   # contiene menos de 5 genes, omitimos dicha ruta metabólica y pasamos a la
   # siguiente iteración del bucle
-  genes <- pathways.gmt[[ruta_metab]]
-  genes_comm <- intersect(genes, rownames(matriz_TPM_norm))
-  if(length(genes_comm) < 5) {
-    print(paste0("La ruta metabólica \"",ruta_metab,"\" presenta menos de 5 genes en la matriz de TPMs normalizada. Omitiendo dicha ruta..."))
+  genes_ruta <- pathways.gmt[[ruta_metab]]
+  genes_compartidos <- intersect(genes_ruta, rownames(matriz_TPM_norm))
+  if (length(genes_compartidos) < 5) {
+    message('La ruta metabólica "',ruta_metab,'" presenta menos de 5 genes en la matriz de TPMs normalizada. Omitiendo dicha ruta...')
     next
   }
 
-  pathway_metabolic_tpm <- matriz_TPM_norm[genes_comm, ]
-  pathway_metabolic_tpm <- pathway_metabolic_tpm[rowSums(pathway_metabolic_tpm)>0,]
+  # Generamos una matriz de TPM de los genes de la ruta en cuestión y
+  # descartamos aquellos con expresión nula en 100% de las células del dataset,
+  # i.e. tasa de dropout global de 100%
+  matriz_TPM_pathway <- matriz_TPM_norm[genes_compartidos, ]
+  matriz_TPM_pathway <- matriz_TPM_pathway[rowSums(matriz_TPM_pathway) > 0,] 
   
-  mean_exp_eachCellType <- apply(pathway_metabolic_tpm, 1, function(x)by(x, all_cell_types, mean))
+  # A partir de la matriz anterior creamos otra de dimensiones linaje celular X
+  # gen de la ruta metabólica que contenga la expresión media de dicho gen en
+  # cada tipo celular
+  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x) by(x, all_cell_types, mean))
 
-  #remove genes which are zeros in any celltype to avoid extreme ratio value
-  keep <- colnames(mean_exp_eachCellType)[colAlls(mean_exp_eachCellType>0.001)]
+  # Eliminamos los genes que tengan expresión nula en cualquier linaje celular para
+  # evitar ratios extremos
+  genes_detectados_todos_tipos_celulares <- colnames(expr_media_por_linaje_cel)[colAlls(expr_media_por_linaje_cel > 0.001)]
 
-  if(length(keep)<3) next
+  # Tras descartar los genes con expresión nula en cualquier linaje celular, si
+  # la ruta metabólica conserva menos de 3 genes, la omitimos y pasamos a la
+  # siguiente iteración del bucle
+  if (length(genes_detectados_todos_tipos_celulares) < 3) {
+    message('La ruta metabólica "',ruta_metab,'" contiene menos de 3 genes con expresión detectada en todos los tipos celulares. Omitiendo dicha ruta...')
+    next
+  }
   
-  #using the loweset value to replace zeros for avoiding extreme ratio value
-  pathway_metabolic_tpm <- pathway_metabolic_tpm[keep,]
-  pathway_metabolic_tpm <- t( apply(pathway_metabolic_tpm,1,function(x) {x[x<=0] <- min(x[x>0]);x} ))
+  # Actualizamos la matriz de TPM con los genes que han pasado el cribado y,
+  # para cada gen de la susodicha, imputamos las células con expresión nula con
+  # el valor mínimo de dicho gen para evitar ratios extremos
+  matriz_TPM_pathway <- matriz_TPM_pathway[genes_detectados_todos_tipos_celulares,]
+  matriz_TPM_pathway <- t(apply(matriz_TPM_pathway, 1, function(x) {x[x <= 0] <- min(x[x > 0]); return(x)}))
 
+  # Actualizamos también la matriz de linaje celular X genes
+  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x)by(x, all_cell_types, mean))
   
-  pathway_number_weight = 1 / gene_pathway_number[keep,]
-  #
-  mean_exp_eachCellType <- apply(pathway_metabolic_tpm, 1, function(x)by(x, all_cell_types, mean))
-  ratio_exp_eachCellType <- t(mean_exp_eachCellType) / colMeans(mean_exp_eachCellType)
-  #exclude the extreme ratios
-  col_quantile <- apply(ratio_exp_eachCellType,2,function(x) quantile(x,na.rm=T))
-  col_q1 <- col_quantile["25%",]
-  col_q3 <- col_quantile["75%",]
-  col_upper <- col_q3 * 3
-  col_lower <- col_q1 / 3
-  outliers <- apply(ratio_exp_eachCellType,1,function(x) {any( (x>col_upper)|(x<col_lower) )} )
+  # Transformamos los valores de expresión TPM de la matriz
+  # `expr_media_por_linaje_cel` a ratios (similar al fold change, expresión
+  # relativa al resto de tipos celulares) y transponemos la matriz para que
+  # tenga dimensiones genes X tipos celulares
+  ratio_exp_por_linaje_cel <- t(expr_media_por_linaje_cel) / colMeans(expr_media_por_linaje_cel)
   
-  if(sum(!outliers) < 3) next
+  pathway_number_weight = 1 / gene_pathway_number[genes_detectados_todos_tipos_celulares,]
   
-  keep <- names(outliers)[!outliers]
-  pathway_metabolic_tpm <- pathway_metabolic_tpm[keep,]
-  pathway_number_weight = 1 / gene_pathway_number[keep,]
-  mean_exp_eachCellType <- apply(pathway_metabolic_tpm, 1, function(x)by(x, all_cell_types, mean))
-  ratio_exp_eachCellType <- t(mean_exp_eachCellType) / colMeans(mean_exp_eachCellType)
-  mean_exp_pathway <- apply(ratio_exp_eachCellType,2, function(x) weighted.mean(x, pathway_number_weight/sum(pathway_number_weight)))
+  
+  # Calculamos para cada linaje celular los cuartiles de los ratios de expresión génica #exclude the extreme ratios
+  cuartiles_ratios_exp_genica <- apply(ratio_exp_por_linaje_cel, 2, function(x) quantile(x, na.rm = T))
+  cuartil1 <- cuartiles_ratios_exp_genica["25%",]
+  limite_inferior <- cuartil1 / 3
+  cuartil3 <- cuartiles_ratios_exp_genica["75%",]
+  limite_superior <- cuartil3 * 3
+  outliers <- apply(ratio_exp_por_linaje_cel, 1, function(x) {any((x < limite_inferior) | (x > limite_superior))})
+  
+  if(sum(!outliers) < 3) {
+    message('Tras eliminar los outliers, la ruta metabólica ',ruta_metab,' conserva menos de 3 genes. Omitiendo dicha ruta...')
+    next
+  }
+  
+  genes_detectados_todos_tipos_celulares <- names(outliers)[!outliers]
+  matriz_TPM_pathway <- matriz_TPM_pathway[genes_detectados_todos_tipos_celulares,]
+  pathway_number_weight = 1 / gene_pathway_number[genes_detectados_todos_tipos_celulares,]
+  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x)by(x, all_cell_types, mean))
+  ratio_exp_por_linaje_cel <- t(expr_media_por_linaje_cel) / colMeans(expr_media_por_linaje_cel)
+  mean_exp_pathway <- apply(ratio_exp_por_linaje_cel,2, function(x) weighted.mean(x, pathway_number_weight/sum(pathway_number_weight)))
   mean_expression_shuffle[ruta_metab, ] <-  mean_exp_pathway[cell_types]
   mean_expression_noshuffle[ruta_metab, ] <-  mean_exp_pathway[cell_types]
     
   ##shuffle 5000 times:  
   ##define the functions 
   group_mean <- function(x){
-    sapply(cell_types,function(y) rowMeans(pathway_metabolic_tpm[,shuffle_cell_types_list[[x]]==y,drop=F]))
+    sapply(cell_types,function(y) rowMeans(matriz_TPM_pathway[,shuffle_cell_types_list[[x]]==y,drop=F]))
   }
   column_weigth_mean <- function(x){
-    apply(ratio_exp_eachCellType_list[[x]],2, function(y) weighted.mean(y, weight_values))
+    apply(ratio_exp_por_linaje_cel_list[[x]],2, function(y) weighted.mean(y, weight_values))
   }
   #####  
   times <- 1:5000
@@ -145,7 +171,7 @@ for(ruta_metab in nombres_pathways){
   shuffle_cell_types_list <- lapply(times,function(x) sample(all_cell_types)) 
   names(shuffle_cell_types_list) <- times
   mean_exp_eachCellType_list <- lapply(times,function(x) group_mean(x))
-  ratio_exp_eachCellType_list <- lapply(times,function(x) mean_exp_eachCellType_list[[x]] / rowMeans(mean_exp_eachCellType_list[[x]]))
+  ratio_exp_por_linaje_cel_list <- lapply(times,function(x) mean_exp_eachCellType_list[[x]] / rowMeans(mean_exp_eachCellType_list[[x]]))
   mean_exp_pathway_list <- lapply(times,function(x) column_weigth_mean(x))
   
   shuffle_results <- matrix(unlist(mean_exp_pathway_list),ncol=length(cell_types),byrow = T) 
@@ -162,7 +188,7 @@ for(ruta_metab in nombres_pathways){
     matriz_pvalues[ruta_metab,c] <- pval
   }
 }
-View(matriz_pvalues)
+
 
 all_NA <- rowAlls(is.na(mean_expression_shuffle))
 mean_expression_shuffle <- mean_expression_shuffle[!all_NA,]
