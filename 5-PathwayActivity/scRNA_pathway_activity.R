19ca38af38b4769eade20594acdb86879f9372d2
gloknar <mipeeeerro@gmail.com>
2021-12-31 23:03:41 +0100

    Finalizado refactorizado y clarificación del bucle principal

--- 5-PathwayActivity/scRNA_pathway_activity.R
+++ 5-PathwayActivity/scRNA_pathway_activity.R
@@ -5,12 +5,13 @@
 # Paquetes y funciones auxiliares
 library(scater)
 source("../utils.R")
+library(ggplot2)
+library(pheatmap)
+
 
 library(stringr)
 library(reshape2)
 library(scales)
-library(pheatmap)
-library(ggplot2)
 library(dplyr)
 library(ggrepel)
 library(RColorBrewer)
@@ -28,8 +29,8 @@ if(!dir.exists(outDir) ) {dir.create(outDir, recursive = TRUE)}
 
 # Leemos el dataset del head_neck/melanoma con la expresión génica imputada
 imputed_sce <- readRDS(file.path("../2-Imputation/datasets",argumento,"imputed_sce.rds"))
-all_cell_types <- as.vector(imputed_sce$cellType)
-cell_types <- unique(all_cell_types)
+etiquetas_celulas <- as.vector(imputed_sce$cellType)
+linajes_celulares <- unique(etiquetas_celulas)
 
 
 # Leemos la matriz de TPM normalizada por deconvolución del dataset del
@@ -61,31 +62,56 @@ gene_pathway_number <- num_of_pathways(ruta_archivo_pathways,
 ###########     1. Cálculo de la actividad de las rutas metabólicas     ###########
 ###################################################################################
 
-# Inicializamos 3 matrices vacías de rutas metabólicas X tipos celulares:
+# Para cada linaje celular de nuestro dataset, vamos a calcular la actividad
+# ponderada de cada ruta y realizaremos un test de permutación aleatoria de
+# tantas permutaciones como queramos (en este caso 5000). Para ello calcularemos
+# la actividad de la ruta en cada linaje celular mediante la expresión media
+# relativa ponderada de los genes que participan en dicha ruta (penalizamos los
+# genes que participan en muchas rutas metabólicas) y compararemos esos valores
+# con los de una distribución nula, resultado de cambiar aleatoriamente las
+# etiquetas de las células.
+
+
+# Primero inicializamos 3 matrices vacías de dimensiones rutas metabólicas X tipos
+# celulares:
 
 # Una para almacenar los p-valores de la actividad metabólica (p-valores
-# calculados con el método de shuffle)
+# calculados con el test de permutación aleatoria)
 matriz_pvalues <- matrix(NA, nrow = length(nombres_pathways), 
-                         ncol = length(cell_types), dimnames = list(nombres_pathways, cell_types))
+                         ncol = length(linajes_celulares), dimnames = list(nombres_pathways, linajes_celulares))
 
-# Otra para #mean ratio of genes in each pathway for each cell type
-mean_expression_shuffle <- matrix(NA, nrow = length(nombres_pathways), 
-                           ncol = length(cell_types), dimnames = list(nombres_pathways, cell_types))
+# Otra para almacenar la actividad real de las rutas metabólicas en los
+# distintos tipos celulares
+distribucion_nula_actividad_pathways <- matrix(NA, nrow = length(nombres_pathways), 
+                           ncol = length(linajes_celulares), dimnames = list(nombres_pathways, linajes_celulares))
 
-# Y otra para
-mean_expression_noshuffle <- matrix(NA, nrow = length(nombres_pathways), 
-                             ncol = length(cell_types), dimnames = list(nombres_pathways, cell_types))
+# Y otra para almacenar la distribución nula sobre la que comparar la actividad
+# real de las rutas
+distribucion_empirica_actividad_pathways <- matrix(NA, nrow = length(nombres_pathways), 
+                             ncol = length(linajes_celulares), dimnames = list(nombres_pathways, linajes_celulares))
 
-# View(mean_expression_shuffle)
-# View(mean_expression_noshuffle)
-# View(matriz_TPM_norm[genes_compartidos, ])
 
+
+# Lo segundo es definir las funciones que usaremos en el bucle a continuación
+group_mean <- function(x){
+  sapply(linajes_celulares, function(y) rowMeans(matriz_TPM_pathway[,etiquetas_celulas_dist_nula[[x]] == y, drop = F]))
+}
+
+column_weigth_mean <- function(x){
+  apply(ratio_exp_por_linaje_cel_permutaciones[[x]], 2, function(y) weighted.mean(y, pesos_genes_pathway))
+}
+
+
+# Ahora iteramos sobre cada ruta metabólica del archivo pathways.gmt para
+# calcular su actividad relativa (a lo "Fold Change") respecto a los distintos
+# linajes celulares y su p-valor por linaje celular
 for (ruta_metab in nombres_pathways) {
   
-  # Comprobamos cuántos genes de la ruta metabólica están presentes tanto en el
-  # archivo pathways.gmt como en la matriz TPM normalizada, y si dicha ruta
-  # contiene menos de 5 genes, omitimos dicha ruta metabólica y pasamos a la
-  # siguiente iteración del bucle
+  ########################################################################
+  # 1ª criba: Comprobamos cuántos genes de la ruta metabólica están presentes
+  # tanto en el archivo pathways.gmt como en la matriz TPM normalizada, y si
+  # dicha ruta contiene menos de 5 genes, omitimos dicha ruta metabólica y
+  # pasamos a la siguiente iteración del bucle
   genes_ruta <- pathways.gmt[[ruta_metab]]
   genes_compartidos <- intersect(genes_ruta, rownames(matriz_TPM_norm))
   if (length(genes_compartidos) < 5) {
@@ -102,15 +128,17 @@ for (ruta_metab in nombres_pathways) {
   # A partir de la matriz anterior creamos otra de dimensiones linaje celular X
   # gen de la ruta metabólica que contenga la expresión media de dicho gen en
   # cada tipo celular
-  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x) by(x, all_cell_types, mean))
+  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x) by(x, etiquetas_celulas, mean))
 
   # Eliminamos los genes que tengan expresión nula en cualquier linaje celular para
   # evitar ratios extremos
   genes_detectados_todos_tipos_celulares <- colnames(expr_media_por_linaje_cel)[colAlls(expr_media_por_linaje_cel > 0.001)]
-
-  # Tras descartar los genes con expresión nula en cualquier linaje celular, si
-  # la ruta metabólica conserva menos de 3 genes, la omitimos y pasamos a la
-  # siguiente iteración del bucle
+  
+  
+  ##########################################################################
+  # 2ª criba: Tras descartar los genes con expresión nula en cualquier linaje
+  # celular, si la ruta metabólica conserva menos de 3 genes, la omitimos y
+  # pasamos a la siguiente iteración del bucle
   if (length(genes_detectados_todos_tipos_celulares) < 3) {
     message('La ruta metabólica "',ruta_metab,'" contiene menos de 3 genes con expresión detectada en todos los tipos celulares. Omitiendo dicha ruta...')
     next
@@ -123,7 +151,7 @@ for (ruta_metab in nombres_pathways) {
   matriz_TPM_pathway <- t(apply(matriz_TPM_pathway, 1, function(x) {x[x <= 0] <- min(x[x > 0]); return(x)}))
 
   # Actualizamos también la matriz de linaje celular X genes
-  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x) by(x, all_cell_types, mean))
+  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x) by(x, etiquetas_celulas, mean))
   
   # Transformamos los valores de expresión TPM de la matriz
   # `expr_media_por_linaje_cel` a ratios (similar al fold change, expresión
@@ -132,12 +160,12 @@ for (ruta_metab in nombres_pathways) {
   ratio_exp_por_linaje_cel <- t(expr_media_por_linaje_cel) / colMeans(expr_media_por_linaje_cel)
   
   
-  
-  # Calculamos para cada linaje celular los cuartiles de los ratios de expresión
-  # génica y eliminamos los genes outliers (con valores de expresión anómalos)
-  # para evitar ratios extremos. Si tras este cribado la ruta metabólica
-  # contiene menos de 3 genes aptos, descartamos su análisis y procedemos a la
-  # siguiente iteración del bucle
+  ##############################################################################
+  # 3ª criba: Calculamos para cada linaje celular los cuartiles de los ratios de
+  # expresión génica y eliminamos los genes outliers (con valores de expresión
+  # anómalos) para evitar ratios extremos. Si tras este cribado la ruta
+  # metabólica contiene menos de 3 genes aptos, descartamos su análisis y
+  # procedemos a la siguiente iteración del bucle
   cuartiles_ratios_exp_genica <- apply(ratio_exp_por_linaje_cel, 2, function(x) quantile(x, na.rm = T))
   cuartil1 <- cuartiles_ratios_exp_genica["25%",]
   limite_inferior <- cuartil1 / 3
@@ -150,61 +178,83 @@ for (ruta_metab in nombres_pathways) {
     next
   }
   
-  # Cribamos los genes por 3a vez y actualizamos la matriz de TPM, la matriz de
-  # linaje celular X genes y la matriz de ratios
+  # Filtramos los genes y actualizamos la matriz de TPM, la matriz de linaje
+  # celular X genes y la matriz de ratios
   genes_detectados_todos_tipos_celulares <- names(outliers)[!outliers]
   matriz_TPM_pathway <- matriz_TPM_pathway[genes_detectados_todos_tipos_celulares,]
-  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x) by(x, all_cell_types, mean))
+  expr_media_por_linaje_cel <- apply(matriz_TPM_pathway, 1, function(x) by(x, etiquetas_celulas, mean))
   ratio_exp_por_linaje_cel <- t(expr_media_por_linaje_cel) / colMeans(expr_media_por_linaje_cel)
-  
   
   # Procedemos a calcular la actividad de la ruta metabólica en cada linaje
   # celular. Para ello calcularemos la expresión media ponderada de los genes
   # que participan en dicha ruta (penalizamos los genes que participan en muchas
   # rutas metabólicas.
   pesos_genes_pathway = 1 / gene_pathway_number[genes_detectados_todos_tipos_celulares,]
+  pesos_genes_pathway <- pesos_genes_pathway/sum(pesos_genes_pathway)
   actividad_ponderada_pathway <- apply(ratio_exp_por_linaje_cel, 2, 
                                  function(x) weighted.mean(x, w = pesos_genes_pathway / sum(pesos_genes_pathway)))
-  mean_expression_shuffle[ruta_metab, ] <-  actividad_ponderada_pathway[cell_types]
-  mean_expression_noshuffle[ruta_metab, ] <-  actividad_ponderada_pathway[cell_types]
-    
-  ##shuffle 5000 times:  
-  ##define the functions 
-  group_mean <- function(x){
-    sapply(cell_types,function(y) rowMeans(matriz_TPM_pathway[,shuffle_cell_types_list[[x]]==y,drop=F]))
-  }
-  column_weigth_mean <- function(x){
-    apply(ratio_exp_por_linaje_cel_list[[x]],2, function(y) weighted.mean(y, weight_values))
-  }
-  #####  
-  times <- 1:5000
-  weight_values <- pesos_genes_pathway/sum(pesos_genes_pathway)
-  shuffle_cell_types_list <- lapply(times,function(x) sample(all_cell_types)) 
-  names(shuffle_cell_types_list) <- times
-  mean_exp_eachCellType_list <- lapply(times,function(x) group_mean(x))
-  ratio_exp_por_linaje_cel_list <- lapply(times,function(x) mean_exp_eachCellType_list[[x]] / rowMeans(mean_exp_eachCellType_list[[x]]))
-  actividad_ponderada_pathway_list <- lapply(times,function(x) column_weigth_mean(x))
   
-  shuffle_results <- matrix(unlist(actividad_ponderada_pathway_list),ncol=length(cell_types),byrow = T) 
-  rownames(shuffle_results) <- times
-  colnames(shuffle_results) <- cell_types
-  for(c in cell_types){
-    if(is.na(mean_expression_shuffle[ruta_metab,c])) next
-    if(mean_expression_shuffle[ruta_metab,c]>1){
-      pval <- sum(shuffle_results[,c] > mean_expression_shuffle[ruta_metab,c]) / 5000 
-    }else if(mean_expression_shuffle[ruta_metab,c]<1){
-      pval <- sum(shuffle_results[,c] < mean_expression_shuffle[ruta_metab,c]) / 5000
+  
+  
+  ###############################################################
+  ###############################################################
+  # Comenzamos el test de permutación aleatoria para calcular la significancia
+  # estadística de la actividad de las rutas metabólicas en cada estirpe
+  # celular. Rellenamos la fila correspondiente de las matrices de dimensiones
+  # ruta metabólica X linaje celular
+  distribucion_nula_actividad_pathways[ruta_metab, ] <-  actividad_ponderada_pathway[linajes_celulares]
+  distribucion_empirica_actividad_pathways[ruta_metab, ] <-  actividad_ponderada_pathway[linajes_celulares]
+  
+  # Generamos la distribución nula (No sacarlo del bucle; los resultados cambian ligeramente respecto al paper original)
+  permutaciones <- 1:5000
+  etiquetas_celulas_dist_nula <- lapply(permutaciones, function(x) sample(etiquetas_celulas))
+  names(etiquetas_celulas_dist_nula) <- permutaciones
+  
+  # Cada permutación consiste en repetir todos los pasos anteriores del presente
+  # bucle con los genes cribados, i.e. 1º calcular expresión media cada gen en
+  # cada linaje celular, 2º calcular su actividad relativa respecto a otros
+  # tipos celulares y 3º hacer la media ponderada de los genes para obtener la
+  # actividad de la ruta metabólica en cada estirpe celular
+  expr_media_por_linaje_cel_permutaciones <- lapply(permutaciones, function(x) group_mean(x))
+  ratio_exp_por_linaje_cel_permutaciones <- lapply(permutaciones,
+      function(x) expr_media_por_linaje_cel_permutaciones[[x]] / rowMeans(expr_media_por_linaje_cel_permutaciones[[x]]))
+  actividad_ponderada_pathway_permutaciones <- lapply(permutaciones, function(x) column_weigth_mean(x))
+  
+  # Agrupamos los resultados de las permutaciones en una matriz
+  matriz_permutaciones <- matrix(unlist(actividad_ponderada_pathway_permutaciones), 
+                            ncol = length(linajes_celulares), byrow = T, 
+                            dimnames = list(permutaciones, linajes_celulares))
+
+  
+  for (c in linajes_celulares) {
+    # Si esta ruta no pasó los cribados (ergo contiene NAs), no la procesamos
+    if (is.na(distribucion_nula_actividad_pathways[ruta_metab,c])) {  
+      next
     }
-    if(pval>0.01) mean_expression_shuffle[ruta_metab, c] <- NA  ### NA is  blank in heatmap
-    matriz_pvalues[ruta_metab,c] <- pval
+    
+    # Calculamos los p-values
+    if (distribucion_nula_actividad_pathways[ruta_metab, c] > 1) {
+      pval <- sum(matriz_permutaciones[, c] > distribucion_nula_actividad_pathways[ruta_metab, c]) / 5000 
+    } else if (distribucion_nula_actividad_pathways[ruta_metab,c] < 1) {
+      pval <- sum(matriz_permutaciones[, c] < distribucion_nula_actividad_pathways[ruta_metab, c]) / 5000
+    }
+    
+    # Si la ruta no es estadísticamente significativa, le asignamos como p-valor
+    # un NA, ya que se muestra blanco en el heatmap
+    if (pval > 0.01) {
+      distribucion_nula_actividad_pathways[ruta_metab, c] <- NA
+    }
+    
+    # Por último rellenamos la matriz de los p-valores
+    matriz_pvalues[ruta_metab, c] <- pval
   }
 }
 
 
-all_NA <- rowAlls(is.na(mean_expression_shuffle))
-mean_expression_shuffle <- mean_expression_shuffle[!all_NA,]
+all_NA <- rowAlls(is.na(distribucion_nula_actividad_pathways))
+distribucion_nula_actividad_pathways <- distribucion_nula_actividad_pathways[!all_NA,]
 #heatmap
-dat <- mean_expression_shuffle
+dat <- distribucion_nula_actividad_pathways
 
 sort_row <- c()
 sort_column <- c()
@@ -227,12 +277,12 @@ color <- colorRampPalette(c("blue","white","red"))(100)
 pheatmap(dat[sort_row,sort_column],cluster_cols = F,cluster_rows = F,color=color,breaks=mybreaks)
 dev.off()
 
-write.table(mean_expression_noshuffle,file=file.path(outDir,"KEGGpathway_activity_noshuffle.txt"),row.names=T,col.names=T,quote=F,sep="\t")
-write.table(mean_expression_shuffle,file=file.path(outDir,"KEGGpathway_activity_shuffle.txt"),row.names=T,col.names=T,quote=F,sep="\t")
+write.table(distribucion_empirica_actividad_pathways,file=file.path(outDir,"KEGGpathway_activity_noshuffle.txt"),row.names=T,col.names=T,quote=F,sep="\t")
+write.table(distribucion_nula_actividad_pathways,file=file.path(outDir,"KEGGpathway_activity_shuffle.txt"),row.names=T,col.names=T,quote=F,sep="\t")
 write.table(matriz_pvalues,file=file.path(outDir,"KEGGpathway_activity_shuffle_pvalue.txt"),row.names=T,col.names=T,quote=F,sep="\t")
 
 #boxplot show the distribution of pathway activity
-scRNA_dat <- as.data.frame(mean_expression_noshuffle)
+scRNA_dat <- as.data.frame(distribucion_empirica_actividad_pathways)
 scRNA_dat$X <- NULL
 
 scRNA_df <- melt(scRNA_dat)
