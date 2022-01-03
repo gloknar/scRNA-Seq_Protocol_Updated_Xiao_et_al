4f68176d6a7c8580631c6f66c2a7c5e703bd33d9
gloknar <mipeeeerro@gmail.com>
2022-01-03 19:49:38 +0100

    .

--- 6-PathwayHeterogeneity/GeneSignature-of-Low_OXPHOS_Glycolysis_Hypoxia.R
+++ 6-PathwayHeterogeneity/GeneSignature-of-Low_OXPHOS_Glycolysis_Hypoxia.R
@@ -5,16 +5,17 @@ library(gtools)
 source("../utils.R")
 options(stringsAsFactors=FALSE)
 
-args <- commandArgs()
-tumor <- args[6]
-outDir <- file.path("dataset",tumor)
+# args <- commandArgs()
+# tumor <- args[6]
+tumor <- "melanoma"
+outDir <- file.path("datasets",tumor)
 if(!dir.exists(outDir) ) dir.create(outDir,recursive=TRUE)
 pathway_file <- "../Data/KEGG_metabolism.gmt"
 pathways <- gmtPathways(pathway_file)
 hallmark_gmt <- '../Data/h.all.v6.1.symbols.gmt'
 hallmarks <- gmtPathways(hallmark_gmt)
 #1. Loading the data
-selected_sce <- readRDS(file.path("../1-ReadData/dataset/",tumor,"selected_sce.rds"))
+selected_sce <- readRDS(file.path("../1-ReadData/datasets/",tumor,"filtered_sce.rds"))
 selected_tumor_sce <- selected_sce[,selected_sce$cellType=="Malignant"]
 selected_tumor_metabolic_sce <- selected_tumor_sce[rowData(selected_tumor_sce)$metabolic,]
 #=========================================================================
