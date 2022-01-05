47aa02b2e558cd0d2ab457d00ebe52d018540186
gloknar <mipeeeerro@gmail.com>
2022-01-05 16:01:05 +0100

    Update impute_tpm.R

--- 2-Imputation/impute_tpm.R
+++ 2-Imputation/impute_tpm.R
@@ -13,7 +13,7 @@ outDir <- file.path("./datasets", argumento) # Carpeta donde guardaremos todos l
 if(!dir.exists(outDir)) {                    # Crea la carpeta ./datasets/<nombre del tumor>/  si no existe
   dir.create(outDir,recursive = TRUE)
 } 
-num_cores <- 6                               # Usar 1 en Windows (scImpute usa mc.apply...)
+num_cores <- 1                               # Usar 1 en Windows (scImpute usa mc.apply...)
 
 # Leemos el dataset del head_neck/melanoma con las células filtradas y a partir
 # de él creamos un objeto `sce` con sólo las células tumorales y otro con sólo
@@ -45,10 +45,8 @@ filtered_sce_nontumor_tpm <- tpm(filtered_sce_nontumor)  # sinónimo de filtered
 # NOTA: Debemos eliminar los niveles no usados de los factores tumor y cellType
 # cada vez que los usemos... Lo hice en el paso 1 del protocolo, pero por algún
 # motivo no se guarda
-filtered_sce_tumor$tumor <- droplevels(filtered_sce_tumor$tumor)
-labels_tumor <- filtered_sce_tumor$tumor
-filtered_sce_nontumor$cellType <- droplevels(filtered_sce_nontumor$cellType)
-labels_nontumor <- filtered_sce_nontumor$cellType
+labels_tumor <- droplevels(filtered_sce_tumor$tumor)
+labels_nontumor <- droplevels(filtered_sce_nontumor$cellType)
 
 # Guardamos las matrices de TPMs en archivos
 write.csv(filtered_sce_tumor_tpm,file.path(outDir,"malignant.tpm"))
