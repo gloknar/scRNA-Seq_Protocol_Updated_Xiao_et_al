07919a2a90ca9f344eb612dc9ea65d661e4392c3
gloknar <mipeeeerro@gmail.com>
2022-01-10 11:44:49 +0100

    Finalizado paso correlaciones

--- 6-PathwayHeterogeneity/OXPHOS_Glycolysis_Hypoxia_Correlation_plot.R
+++ 6-PathwayHeterogeneity/OXPHOS_Glycolysis_Hypoxia_Correlation_plot.R
@@ -61,21 +61,21 @@ gene_pathway_num <- num_of_pathways(ruta_archivo_pathways, intersect(unlist(path
 
 
 # Calculamos la expresión media de los genes de cada proceso metabólico
-all_gene_exp <- assay(tumor_sce, "exprs")
+all_genes_expr <- assay(tumor_sce, "exprs")
 
-oxphos_exp <- all_gene_exp[rownames(all_gene_exp) %in% genes_OXPHOS,]
-glycolysis_exp <- all_gene_exp[rownames(all_gene_exp) %in% genes_glicolisis,]
-hypoxia_exp <- all_gene_exp[rownames(all_gene_exp) %in% genes_hipoxia,]
+expr_oxphos <- all_genes_expr[rownames(all_genes_expr) %in% genes_OXPHOS,]         # Matriz TPM de los genes que participan en OXPHOS
+expr_glicolisis <- all_genes_expr[rownames(all_genes_expr) %in% genes_glicolisis,] # Ídem para glicólisis
+expr_hipoxia <- all_genes_expr[rownames(all_genes_expr) %in% genes_hipoxia,]       # Y para hipoxia
 
-oxphos <- colMeans(as.matrix(oxphos_exp), na.rm = T)
-glycolysis <- colMeans(as.matrix(glycolysis_exp), na.rm = T)
-hypoxia <- colMeans(as.matrix(hypoxia_exp), na.rm = T)
-data <- data.frame(OXPHOS = oxphos, Glycolysis = glycolysis, Hypoxia = hypoxia)
+oxphos <- colMeans(as.matrix(expr_oxphos), na.rm = T)                          # Para cada célula, calculamos la expresion media de los genes que participan en la ruta metabólica OXHPOS
+glicolisis <- colMeans(as.matrix(expr_glicolisis), na.rm = T)                  # Lo mismo para glicólisis
+hipoxia <- colMeans(as.matrix(expr_hipoxia), na.rm = T)                        # Y para hipoxia
+data <- data.frame(OXPHOS = oxphos, Glicolisis = glicolisis, Hipoxia = hipoxia)   # Agregamos todo en un dataframe
 
 
 # Calculamos la matriz de correlación para las 3 rutas metabólicas
 print("Correlación de las rutas metabólicas de interés:")
-print(cor(data, method = "pearson"))  # Usa pearson porque tenemos más de 30 células, y podemos asumir que ambas distribuciones se aproximan a una normal
+print(cor(data, method = "pearson"))  # Usa pearson porque tenemos más de 30 células, y podemos asumir que ambas distribuciones se aproximan a una normal... puedes comprobarlo si creas un objeto aov y lo ploteas, te saldra el qqplot
 
 
 # Graficamos la regresión lineal para OXPHOS y glicólisis
@@ -83,18 +83,23 @@ data_min <- 0   # Rangos máximos y mínimos de dataframe
 data_max <- 4
 
 # Calculamos la ecuación de la regresión lineal para mostrarla en el gráfico
-a <- lm(Glycolysis ~ OXPHOS, data)
+a <- lm(Glicolisis ~ OXPHOS, data)
 b <- summary(a)
-formula <- paste0("y = ",round(a$coefficients[[1]], 3), " + ", round(a$coefficients[[2]], 3), "x ;  R^2 = ", round(b$r.squared,3))
+formula1 <- paste0("y = ",round(a$coefficients[[1]], 2), " + ", round(a$coefficients[[2]], 2), "x ;  R = ", round(sqrt(b$adj.r.squared), 2))
+p_valor <- pf(q = b$fstatistic[[1]], df1= b$fstatistic[[2]], 
+              df2= b$fstatistic[[3]], lower.tail = F)  # Ya que el summary de un objeto lm nos enseña el p-valor pero no nos los devuelve de manera programática, lo calculamos a partir del estadístico F
+formula2 <- paste0("p-value = ", format(p_valor, digits = 3))
+
 
 # Generamos el gráfico y lo guardamos en un pdf
-p = ggplot(data, aes(x = OXPHOS, y = Glycolysis)) + 
+p = ggplot(data, aes(x = OXPHOS, y = Glicolisis)) + 
     geom_point(size = 0.5) +
     geom_smooth(method = "lm", color = "red") +
     xlim(data_min, data_max) + ylim(data_min, data_max) +
     theme_classic()  + theme(aspect.ratio = 0.8) +
-    labs(x = "OXPHOS", y = "Glycolysis") +
-    geom_text(x = 1, y = 3.5, label = formula) +
+    labs(x = "OXPHOS", y = "Glicolisis") +
+    geom_text(x = 1, y = 3.5, label = formula1) +
+    geom_text(x = .75, y = 3, label = formula2) +
     theme(axis.line = element_line(size = 0.3, colour = "black"),
           axis.ticks = element_line(size = 0.3, color = "black"),
           axis.text.x = element_text(size = 6),
@@ -102,24 +107,29 @@ p = ggplot(data, aes(x = OXPHOS, y = Glycolysis)) +
           axis.title.x = element_text(size = 8),
           axis.title.y = element_text(size = 8))
 
-ggsave(filename = file.path(outDir,"malignant_oxphos_glycolysis.pdf"), p, 
+ggsave(filename = file.path(outDir,"malignant_Oxphos_Glicolisis.pdf"), p, 
        device = "pdf", width = 6, height = 4, units = "in",
        useDingbats = FALSE)   # Evitamos usar la fuente Dingbats porque según la documentación de ggplot2, a veces da problemas
 
 
 
 # Ídem para OXPHOS e hipoxia
-a <- lm(Hypoxia ~ OXPHOS, data)
+a <- lm(Hipoxia ~ OXPHOS, data)
 b <- summary(a)
-formula <- paste0("y = ",round(a$coefficients[[1]], 3), " + ", round(a$coefficients[[2]], 3), "x ;  R^2 = ", round(b$r.squared,3))
+formula1 <- paste0("y = ",round(a$coefficients[[1]], 2), " + ", round(a$coefficients[[2]], 2), "x ;  R = ", round(sqrt(b$adj.r.squared), 2))
+p_valor <- pf(q = b$fstatistic[[1]], df1= b$fstatistic[[2]], 
+              df2= b$fstatistic[[3]], lower.tail = F)
+formula2 <- paste0("p-value = ", format(p_valor, digits = 3))
 
-p = ggplot(data, aes(x = OXPHOS, y = Hypoxia)) + 
+
+p = ggplot(data, aes(x = OXPHOS, y = Hipoxia)) + 
     geom_point(size = 0.5) +
     geom_smooth(method = "lm", color = "red") +
     xlim(data_min, data_max) + ylim(data_min, data_max) +
     theme_classic()  + theme(aspect.ratio = 0.8) +
-    labs(x = "OXPHOS", y = "Hypoxia") +
-    geom_text(x = 1, y = 3, label = formula) +
+    labs(x = "OXPHOS", y = "Hipoxia") +
+    geom_text(x = 1, y = 3.5, label = formula1) +
+    geom_text(x = .75, y = 3, label = formula2) +
     theme(axis.line = element_line(size = 0.3, colour = "black"),
           axis.ticks = element_line(size = 0.3, color = "black"),
           axis.text.x = element_text(size = 6),
@@ -127,23 +137,29 @@ p = ggplot(data, aes(x = OXPHOS, y = Hypoxia)) +
           axis.title.x = element_text(size = 8),
           axis.title.y = element_text(size = 8))
   
-ggsave(filename = file.path(outDir,"malignant_oxphos_hypoxia.pdf"), p, 
+ggsave(filename = file.path(outDir,"malignant_Oxphos_Hipoxia.pdf"), p, 
        device = "pdf", width = 6, height = 4, units = "in",
        useDingbats = FALSE)
 
 
 # Ídem para glicólisis e hipoxia
-a <- lm(Hypoxia ~ Glycolysis, data)
+a <- lm(Hipoxia ~ Glicolisis, data)
 b <- summary(a)
-formula <- paste0("y = ",round(a$coefficients[[1]], 3), " + ", round(a$coefficients[[2]], 3), "x ;  R^2 = ", round(b$r.squared,3))
+formula1 <- paste0("y = ",round(a$coefficients[[1]], 2), " + ", round(a$coefficients[[2]], 2), "x ;  R = ", round(sqrt(b$adj.r.squared), 2))
+p_valor <- pf(q = b$fstatistic[[1]], df1= b$fstatistic[[2]], 
+              df2= b$fstatistic[[3]], lower.tail = F)
+formula2 <- paste0("p-value = ", format(p_valor, digits = 3))
 
-p = ggplot(data, aes(x = Glycolysis, y = Hypoxia)) + 
+
+
+p = ggplot(data, aes(x = Glicolisis, y = Hipoxia)) + 
     geom_point(size = 0.5) +
     geom_smooth(method = "lm",color = "red") +
     xlim(data_min, data_max) + ylim(data_min, data_max) +
-    labs(x = "Glycolysis", y = "Hypoxia") +
+    labs(x = "Glicolisis", y = "Hipoxia") +
     theme_classic()  + theme(aspect.ratio = 0.8) +
-    geom_text(x = 1, y = 3, label = formula) +
+    geom_text(x = 1, y = 3.5, label = formula1) +
+    geom_text(x = .75, y = 3, label = formula2) +
     theme(axis.line = element_line(size = 0.3, colour = "black"),
           axis.ticks = element_line(size = 0.3, color = "black"),
           axis.text.x = element_text(size = 6),
@@ -151,25 +167,25 @@ p = ggplot(data, aes(x = Glycolysis, y = Hypoxia)) +
           axis.title.x = element_text(size = 8),
           axis.title.y = element_text(size = 8))
 
-ggsave(filename = file.path(outDir,"malignant_glycolysis_hypoxia.pdf"), p, 
+ggsave(filename = file.path(outDir,"malignant_Glicolisis_Hipoxia.pdf"), p, 
        device = "pdf", width = 6, height = 4, units = "in",
        useDingbats = FALSE)
 
 
-# #correlation in each tumor
+# # Correlación entre cada tumor (= paciente)
 # for( i in neoplasias){
-#   each_exp <- assay(tumor_sce[,tumor_sce$tumor==i],"exprs")
-#   oxphos_exp <- each_exp[rownames(each_exp)%in% genes_OXPHOS,]
-#   glycolysis_exp <- each_exp[rownames(each_exp)%in% genes_glicolisis,]
-#   hypoxia_exp <- each_exp[rownames(each_exp)%in% genes_hipoxia,]
-  
-#   oxphos <- colMeans(as.matrix(oxphos_exp),na.rm=T)
-#   glycolysis <- colMeans(as.matrix(glycolysis_exp),na.rm=T)
-#   hypoxia <- colMeans(as.matrix(hypoxia_exp),na.rm=T)
-#   data <- data.frame(OXPHOS=oxphos,Glycolysis=glycolysis,Hypoxia=hypoxia)
-#   print(paste0("correlations in patient:",i))
+#   each_expr <- assay(tumor_sce[,tumor_sce$tumor == i], "exprs")
+#   expr_oxphos <- each_expr[rownames(each_expr) %in% genes_OXPHOS,]
+#   expr_glicolisis <- each_expr[rownames(each_expr) %in% genes_glicolisis,]
+#   expr_hipoxia <- each_expr[rownames(each_expr) %in% genes_hipoxia,]
+# 
+#   oxphos <- colMeans(as.matrix(expr_oxphos), na.rm = T)
+#   glicolisis <- colMeans(as.matrix(expr_glicolisis), na.rm = T)
+#   hipoxia <- colMeans(as.matrix(expr_hipoxia), na.rm = T)
+#   data <- data.frame(OXPHOS = oxphos, Glicolisis = glicolisis, Hipoxia = hipoxia)
+#   print(paste0("correlacones entre pacientes:",i))
 #   print(cor(data))
-# }
+#  }
 
 
 # Limpieza RAM
@@ -184,22 +200,46 @@ gc(verbose = F)
 ###########     2. Correlación entre hipoxia, glicólisis y OXPHOS en células sanas     ###########
 ##################################################################################################
 
-healthy_sce <- filtered_sce[,filtered_sce$cellType!="Malignant"]
+# Hacemos un subset del objeto sce original para quedarnos sólo con las células
+# sanas
+healthy_sce <- filtered_sce[,filtered_sce$cellType != "Malignant"]
+healthy_sce$cellType <-  droplevels(healthy_sce$cellType)
 cell_types <- unique(healthy_sce$cellType)
-cor_matrix <- matrix(NA,nrow=length(cell_types),ncol=3,dimnames = list(cell_types,c("oxphos_glycolosis","oxphos_hypoxia","glycolysis_hypoxia")))
-for(c in cell_types){
-  each_exp <- assay(healthy_sce[,healthy_sce$cellType==c],"exprs")
-  oxphos_exp <- each_exp[rownames(each_exp)%in% genes_OXPHOS,]
-  glycolysis_exp <- each_exp[rownames(each_exp)%in% genes_glicolisis,]
-  hypoxia_exp <- each_exp[rownames(each_exp)%in% genes_hipoxia,]
+
+# Limpieza RAM, ya no necesitamos el objeto filtered_sce
+rm(filtered_sce)
+gc(verbose = F)
+
+
+
+# Inicializamos una matriz de correlaciones vacía de tipos celulares X
+# correlaciones
+matriz_corr <- matrix(NA, nrow = length(cell_types), ncol = 3,
+                     dimnames = list(cell_types, c("oxphos_glicolosis","oxphos_hipoxia","glicolisis_hipoxia")))
+
+
+for (c in cell_types) {
+  each_expr <- assay(healthy_sce[,healthy_sce$cellType == c], "exprs")   # Obtenemos la matriz TPM de las células del linaje de interés
+  expr_oxphos <- each_expr[rownames(each_expr) %in% genes_OXPHOS,]         # Obtenemos de ahi la matriz TPM de los genes de OXPHOS
+  expr_glicolisis <- each_expr[rownames(each_expr) %in% genes_glicolisis,] # Ídem para glicólisis
+  expr_hipoxia <- each_expr[rownames(each_expr) %in% genes_hipoxia,]       # Y para hipoxia
   
-  oxphos <- colMeans(as.matrix(oxphos_exp),na.rm=T)
-  glycolysis <- colMeans(as.matrix(glycolysis_exp),na.rm=T)
-  hypoxia <- colMeans(as.matrix(hypoxia_exp),na.rm=T)
-  data <- data.frame(OXPHOS=oxphos,Glycolysis=glycolysis,Hypoxia=hypoxia)
-  #calculate correlation
-  cor_matrix[c,1] <- cor(data[,1:2])[1,2]
-  cor_matrix[c,2] <- cor(data[,c(1,3)])[1,2]
-  cor_matrix[c,3] <- cor(data[,2:3])[1,2]
+  # Para cada célula, calculamos la expresion media (i.e. actividad de la ruta)
+  # de los genes que participan en la ruta metabólica OXHPOS, glicólisis e
+  # hipoxia, respectivamente
+  oxphos <- colMeans(as.matrix(expr_oxphos), na.rm = T)       
+  glicolisis <- colMeans(as.matrix(expr_glicolisis), na.rm = T)
+  hipoxia <- colMeans(as.matrix(expr_hipoxia), na.rm = T)
+  data <- data.frame(OXPHOS = oxphos, Glicolisis = glicolisis, Hipoxia = hipoxia)  # Agregamos todo en un dataframe
+  
+  # Rellenamos la matriz de correlaciones previamente inicializada
+  matriz_corr[c,1] <- cor(data[, c("OXPHOS", "Glicolisis")])[1,2]
+  matriz_corr[c,2] <- cor(data[,c("OXPHOS", "Hipoxia")])[1,2]
+  matriz_corr[c,3] <- cor(data[,c("Glicolisis", "Hipoxia")])[1,2]
 }
-write.table(cor_matrix,file.path(outDir,"non-malignant_Oxphos_Glycolysis_Hypoxia_cor.txt"),row.names = T,col.names=T,sep="\t")
+
+# Guardamos las correlaciones entre las 3 rutas de interés en cada linaje
+# celular en un archivo tsv
+write.table(matriz_corr, 
+    file.path(outDir,"non-malignant_Oxphos_Glicolisis_Hipoxia_corr.tsv"),
+    row.names = T, col.names = NA, sep = "\t")
